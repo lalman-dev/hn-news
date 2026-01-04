02d3863cc88b99332e2793c46f7c43111dcf0a25
Lalman <lalmanchaudhary2@gmail.com>
2026-01-04 12:18:51 +0530

    fix: delay rendering of toggle to fix mismatch of themetoggle with system preference in initial UI state.

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState<boolean>(false);
   useEffect(() => {
     setMounted(true);
@@ -13,13 +13,15 @@ export default function ThemeToggle() {
 
   if (!mounted) return null;
 
-  const isDark = theme === "dark";
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
     <button
       type="button"
-      aria-label={`Switch to ${isDark ? "light" : "dark"} theme`}
-      onClick={() => setTheme(isDark ? "light" : "dark")}
+      aria-label={`Switch to ${
+        currentTheme === "dark" ? "light" : "dark"
+      } theme`}
+      onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
       className="inline-flex items-center justify-center rounded-lg p-2 border border-gray-500 dark:border-gray-700
         bg-slate-300 dark:bg-gray-900
         text-gray-900 dark:text-gray-100
@@ -27,7 +29,7 @@ export default function ThemeToggle() {
         focus:outline-none focus:ring-2 focus:ring-orange-400
         transition-colors"
     >
-      {isDark ? <Sun size={18} /> : <Moon size={18} />}
+      {currentTheme === "dark" ? <Sun size={18} /> : <Moon size={18} />}
     </button>
   );
 }
