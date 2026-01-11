898803fe121890fff35333afefcefcddb4bf271f
Lalman <lalmanchaudhary2@gmail.com>
2026-01-11 11:28:45 +0530

    refactor background element to reuse it across pages

---
+++ app/components/Background.tsx
@@ -0,0 +1,10 @@
+export default function Background() {
+  return (
+    <img
+      src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/hero/bg-gradient-2.png"
+      alt="background-image"
+      aria-hidden="true"
+      className="absolute inset-0 -z-10 size-full"
+    />
+  );
+}
